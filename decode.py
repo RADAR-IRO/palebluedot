46cc10c105ed919e415be3778a54210b79003b42
Mattéo Delabre <git.matteo@delab.re>
2023-04-17 23:11:47 -0400

    Ajout de documentation pour les fonctions du décodeur

--- decode.py
+++ decode.py
@@ -8,23 +8,46 @@ import sys
 import logging
 
 
+# Number of symbols in a complete APT line
 line_width = 1040
+
+# Number of APT lines transmitted every second
 lines_per_second = 4
+
+# Carrier frequency used to transmit the signal
 carrier_frequency = 2400
+
+# Synchronization pattern for channel A
 sync_a_pattern = "000011001100110011001100110011000000000"
+
+# Synchronization pattern for channel B
 sync_b_pattern = "000011100111001110011100111001110011100"
 
 
 def read_signal(path):
+    """
+    Read a signal from a WAV file.
+
+    :param path: path to the file.
+    :return: sampling rate and list of samples.
+    """
     rate, signal = scipy.io.wavfile.read(path)
 
     if len(signal.shape) == 2:
+        # Merge all channels into one
         signal = signal.mean(axis=1)
 
     return rate, signal.astype("float")
 
 
-def amplitude_demod(signal, rate):
+def amplitude_demod(rate, signal):
+    """
+    Demodulate an amplitude-modulated signal.
+
+    :param rate: signal sampling rate.
+    :param signal: list of samples.
+    :return: demodulated signal.
+    """
     lowpass = scipy.signal.butter(
         N=5,
         Wn=carrier_frequency * 1.1,
@@ -36,6 +59,13 @@ def amplitude_demod(signal, rate):
 
 
 def gen_sync_signal(pattern, samples_per_symbol):
+    """
+    Generate a square synchronization signal from a pattern.
+
+    :param pattern: binary synchronization pattern.
+    :param samples_per_symbol: number of samples for each sync symbol.
+    :return: generated synchronization signal.
+    """
     length = int(samples_per_symbol * len(pattern))
     return np.array([
         int((pattern + "0")[round(i / samples_per_symbol)])
@@ -44,6 +74,14 @@ def gen_sync_signal(pattern, samples_per_symbol):
 
 
 def find_syncs(levels, sync_signal, samples_per_symbol):
+    """
+    Look for synchronization patterns inside a signal.
+
+    :param levels: demodulated signal.
+    :param sync_signal: signal to look for.
+    :param samples_per_symbol: number of samples for each sync symbol.
+    :return: list of candidate synchronization times.
+    """
     corr = correlate_template(levels, sync_signal)
     peaks, _ = scipy.signal.find_peaks(
         corr,
@@ -54,6 +92,14 @@ def find_syncs(levels, sync_signal, samples_per_symbol):
 
 
 def image_from_signal(levels, syncs, samples_per_symbol):
+    """
+    Reconstruct an image from an APT signal.
+
+    :param levels: demodulated signal.
+    :param syncs: synchronization times.
+    :param samples_per_symbol: number of samples for each sync symbol.
+    :return: decoded image.
+    """
     first_sync = syncs[0]
     last_sync = syncs[-1]
 
@@ -71,27 +117,30 @@ def image_from_signal(levels, syncs, samples_per_symbol):
     return Image.fromarray((data / 6000 * 255).round().clip(0, 255).astype("uint8"))
 
 
-def decode(input_path):
-    logging.info("Loading file")
-    rate, signal = read_signal(input_path)
+def apt_decode(rate, signal):
+    """
+    Decode both image channels from an APT signal.
+
+    :param rate: signal sampling rate.
+    :param signal: list of samples.
+    :return: two images, one for each channel.
+    """
     samples_per_symbol = rate / (line_width * lines_per_second)
 
     logging.info("Demodulating signal")
-    levels = amplitude_demod(signal, rate)
+    levels = amplitude_demod(rate, signal)
 
-    logging.info("Finding syncs for channel A")
-    sync_a_signal = gen_sync_signal(sync_a_pattern, samples_per_symbol)
-    syncs_a = find_syncs(levels, sync_a_signal, samples_per_symbol)
-    logging.info("Decoding channel A")
-    image_a = image_from_signal(levels, syncs_a, samples_per_symbol)
+    channels = []
 
-    logging.info("Finding syncs for channel B")
-    sync_b_signal = gen_sync_signal(sync_b_pattern, samples_per_symbol)
-    syncs_b = find_syncs(levels, sync_b_signal, samples_per_symbol)
-    logging.info("Decoding channel B")
-    image_b = image_from_signal(levels, syncs_b, samples_per_symbol)
+    for channel, pattern in (("A", sync_a_pattern), ("B", sync_b_pattern)):
+        logging.info(f"Finding syncs for channel {channel}")
+        sync_signal = gen_sync_signal(pattern, samples_per_symbol)
+        syncs = find_syncs(levels, sync_signal, samples_per_symbol)
 
-    return image_a, image_b
+        logging.info(f"Decoding channel {channel}")
+        channels.append(image_from_signal(levels, syncs, samples_per_symbol))
+
+    return channels
 
 
 if __name__ == "__main__":
@@ -102,7 +151,10 @@ if __name__ == "__main__":
     logging.basicConfig(format='[%(asctime)s] %(message)s', level=logging.INFO)
     input_path = sys.argv[1]
     base_path = input_path.removesuffix(".wav")
-    image_a, image_b = decode(input_path)
+
+    logging.info("Loading file")
+    rate, signal = read_signal(input_path)
+    image_a, image_b = apt_decode(rate, signal)
 
     logging.info(f"Saving images to {base_path}_*.png")
     image_a.save(base_path + "_a.png")
